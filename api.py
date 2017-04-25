d23dd5f624150bcb3d964dab3ab657240e7bf791
Minghua Jiang <61221630@qq.com>
2017-04-25 16:13:56 -0400

    a

--- api.py
+++ api.py
@@ -64,7 +64,7 @@ def update_dataset():
 
 def init_spark_context():
     # load spark context
-    conf = SparkConf().setAppName("movielens_recommendation").setMaster("spark-master")
+    conf = SparkConf().setAppName("movielens_recommendation").setMaster("172.31.22.90")
     # IMPORTANT: pass additional Python modules to each worker
     sc = SparkContext(conf=conf, pyFiles=['engine.py'])
     return sc
